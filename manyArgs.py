39c232fa5629dd6a9c0450c1b9ee81e46f40c720
haninsuk0116 <80086307+haninsuk0116@users.noreply.github.com>
2021-05-14 15:49:31 +0900

    Add files via upload

---
+++ manyArgs.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 15:08:31 2021
+
+@author: Mac_1
+"""
+
+def print_kwargs(**kwargs):
+    print(kwargs)
+    
+print_kwargs(a=1, abc="def", d=3.14159)
+
